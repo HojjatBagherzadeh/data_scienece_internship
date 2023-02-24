56c847f5c04e5efaebd17cca105464622d4fcba9
HojjatBagherzadeh <49332134+HojjatBagherzadeh@users.noreply.github.com>
2023-02-24 11:26:53 +0330

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.header('Hello Data Science Internship 2023!.')
+st.header('Hello Data Science Internship 2023!. \n My name is Hojjat Bagherzadeh. Glad to be working with you.')
